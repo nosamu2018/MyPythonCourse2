54e8b5b425546bb76f59e6280f3adf88e122b0d5
nosamu2018 <nosamu2018@gmail.com>
2019-09-24 11:15:42 -0700

    Playing with numerics and simple operators

---
+++ scratchpad1.py
@@ -0,0 +1,26 @@
+temp = 70
+print(type(temp))
+
+degrees = 350.5
+print (type(degrees))
+
+date = 'Monday'
+print(type(date))
+
+group = [1,2,3]
+print(type(group))
+
+#prints true or false
+print(bool(2-2))
+
+#playing with integers
+#print 1000000000 from 1000_000_000
+h = 1000_000_000
+print(h)
+
+#Operators with integers
+x,y=11,5
+print("x+4 is", x+4)
+print("x//y is" x//y)
+print("12%5 is" 12%5)
+print ("x**y is" x**y)
