e838525801da754a20f6ff3a1ab322193934b8ad
nosamu2018 <nosamu2018@gmail.com>
2019-09-26 03:53:57 -0700

    update to previous

--- CH03/CH04/num_list.py
+++ CH03/CH04/num_list.py
@@ -3,10 +3,20 @@ some_nums = [2,6,4,2,22,54,12,8,-1]
 len(some_nums)
 #Print the sum using a function called sum.
 print('the sum of the list is: ',sum(some_nums))
-print (some_nums[3])
+print (some_nums[0])
 
 highest_num = some_nums[0]
 for x in some_nums:
-    if x > highest_num
+    if x > highest_num:
         highest_num = x
-    print('the highest number is 'highest_num)
+print('the highest number is ', highest_num)
+#Now change some_nums so that every other items is the number 11.
+
+#You cannot change values while in a for loop that is iterating over the collection.
+#You can though, create a for loop with a range, and use its counter as an index
+#some_nums = [2,6,4,2,22,54,12,8,-1]
+for x in range(len(some_nums)-1):
+    print (x)
+    if (x%2==0):
+        some_nums [x] = 11
+    print(some_nums) 
